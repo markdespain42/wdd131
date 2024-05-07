eb44e3ee05aaef75ed6a86204b143553555e79e3
markdespain42 <des21006@byui.edu>
2024-05-07 13:53:15 -0600

    Editted mission statemnt and created media queries.

--- mission/scripts/mission.js
+++ mission/scripts/mission.js
@@ -2,24 +2,20 @@
 const themeSelector = document.getElementById("themeSelector");
 themeSelector.addEventListener("click", changeTheme)
 
-const logoImage = document.getElementById("logo");
-const otherImage = document.getElementById("logo-dark");
-
-
+// const darkImage = document.getElementById("logo").setAttribute("src");
+// const lightImage = document.getElementById("logo").setAttribute("src");
+const logo = document.querySelector(".logo");
 function changeTheme() {
         const selectedTheme = themeSelector.value;
         if (themeSelector.value == "light") {
             document.body.classList.remove("dark");
             document.body.classList.add("light");
 
-            logoImage.classList.add("logo");
-            otherImage.classList.remove("logo-dark");
+            logo.src = "byui-logo_blue.webp"
         } else if (selectedTheme == "dark") {
             document.body.classList.remove("light");
             document.body.classList.add("dark");
 
-            otherImage.classList.add("logo-dark");
-            logoImage.classList.remove("logo");
-
+            logo.src = "byui-logo_white.png"
         }
 }
